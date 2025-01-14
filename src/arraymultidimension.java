a1892e2a592b35185e8a18c8f2b27703681727b3
DELL <arpitalungare22@gmail.com>
2025-01-14 21:54:11 +0530

    first git

---
+++ src/arraymultidimension.java
@@ -0,0 +1,13 @@
+public class arraymultidimension {
+    public static void main(String[] args) {
+        int []marks={1,2,3};
+        try{
+        System.out.println(marks[3]);
+    } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("continue the next flow");
+        }
+
+
+}
