e5fb623a7a01da7b54905b038a0721a13dc84497
DELL <arpitalungare22@gmail.com>
2025-01-14 21:53:28 +0530

    first git

---
+++ src/ArrayPractis/StringPractice.java
@@ -0,0 +1,8 @@
+package ArrayPractis;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String abc="Arpita";
+        System.out.println(abc.toUpperCase());
+    }
+}
